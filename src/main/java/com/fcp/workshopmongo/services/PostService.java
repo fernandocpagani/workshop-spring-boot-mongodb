bb5d6b5dd6f8aaa246705e5aaeb6b73e6f6d471b
Fernando Codolo Pagani <fernandocpagani@hotmail.com>
2023-10-18 22:49:52 -0300

    Obtendo um post por id

---
+++ src/main/java/com/fcp/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.fcp.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fcp.workshopmongo.domain.Post;
+import com.fcp.workshopmongo.repository.PostRepository;
+import com.fcp.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
